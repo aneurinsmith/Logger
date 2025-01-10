cac41687c11e5483cc1085ead9c50f360e3ce8fc
Aneurin Smith <aneurinsmith@yahoo.com>
2025-01-10 21:13:21 +0000

    Added details to terminal test scenario

--- tests/terminal_sink_test.cpp
+++ tests/terminal_sink_test.cpp
@@ -1,7 +1,22 @@
 
 #include <logger.h>
 
+LOG::Logger logger;
+
 int main()
 {
+    logger.add_sink(LOG::terminalSink());
+    LOG::set_level(NONE);
+
+    LOG::trace("Trace");
+    LOG::print("");
+
+    LOG::trace("Trace");
+    LOG::debug("Debug");
+    LOG::info("Info");
+    LOG::warn("Warn");
+    LOG::error("Error");
+    LOG::fatal("Fatal");
+
     return 0;
 }
