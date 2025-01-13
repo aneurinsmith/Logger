f7c3713a4b07df1b6ffb03edc2f71e3a03aa4a52
Aneurin Smith <aneurinsmith@yahoo.com>
2025-01-13 23:56:34 +0000

    ostream_sink now uses basesink constructor. Factories updated to send the level and format information to the sink

--- include/logger/ostream_sink.h
+++ include/logger/ostream_sink.h
@@ -7,14 +7,17 @@ namespace LOG
 {
 	class OStreamSink : public basesink
 	{
+	public:
+		using basesink::basesink;
+
 	protected:
 		void write(std::string msg);
 
 	};
 
-	inline std::shared_ptr<OStreamSink> oStreamSink()
+	inline std::shared_ptr<OStreamSink> oStreamSink(LOG::Level lvl = (Level)0, std::string fmt = "%Y/%m/%d %H:%M:%S.%f")
 	{
-		auto sink = std::make_shared<OStreamSink>();
+		auto sink = std::make_shared<OStreamSink>(lvl, fmt);
 		return sink;
 	}
 }
