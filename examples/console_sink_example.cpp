bcf4f508b22306258e73d2c7594ce68a5b7e4322
Aneurin Smith <aneurinsmith@yahoo.com>
2025-02-13 22:29:37 +0000

    Adjusted examples to use ostreamsink instead of cout

--- examples/console_sink_example.cpp
+++ examples/console_sink_example.cpp
@@ -1,26 +1,30 @@
 
 #include <logger.h>
-#include <chrono>
+#include <timer.h>
 #include <thread>
 #include <iostream>
 
-LOG::Logger logger;
+LOG::Logger consoleLogger;
+LOG::Logger ossLogger(LOG::oStreamSink());
 
 int main()
 {
-    std::cout << "app start" << std::endl;
-    logger.add_sink(LOG::consoleSink());
-    logger.add_sink(LOG::consoleSink());
+    ossLogger.print(LOG::TRACE, "app start");
+    consoleLogger.add_sink(LOG::consoleSink());
+    consoleLogger.add_sink(LOG::consoleSink());
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    std::cout << "we are now looping" << std::endl;
+    LOG::Timer timer;
+    timer.start();
+    ossLogger.print(LOG::TRACE, "we are now looping");
     for (int i = 0; i < 10000; i++) {
         std::string msg = "Terminal window message ";
         msg += std::to_string(i);
-        logger.print(LOG::TRACE, msg);
+        consoleLogger.print(LOG::TRACE, msg);
     }
-    std::cout << "done" << std::endl;
+    ossLogger.print(LOG::TRACE, "done");
+    ossLogger.print(LOG::TRACE, "time taken (ms): " + std::to_string(timer.stop()));
 
     std::this_thread::sleep_for(std::chrono::minutes(10));
 
--- examples/fstream_sink_example.cpp
+++ examples/fstream_sink_example.cpp
@@ -8,7 +8,6 @@ int main()
 {
     logger.add_sink(LOG::fStreamSink());
     for (int i = 0; i < 100; i++) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
         LOG::debug("Hello World!");
     }
 
