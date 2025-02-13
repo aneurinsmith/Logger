24d76ea0de1355dbc61096495bb154c705bfa4cc
Aneurin Smith <aneurinsmith@yahoo.com>
2025-02-13 22:08:17 +0000

    Readded ansi color codes to win32

--- src/sinks/ostream_sink.cpp
+++ src/sinks/ostream_sink.cpp
@@ -12,21 +12,40 @@ namespace LOG
 
 	void OStreamSink::write(Message msg)
 	{
+	#ifdef win32
+		std::cout << "\033[90m";
+	#endif
+
+		std::cout << msg.get_tsString();
+
+	#ifdef win32
 		switch (msg.get_lvl()) {
-		case LOG::TRACE: 
+		case LOG::TRACE:
 			break;
 		case LOG::DEBUG:
+			std::cout << "\033[97m";
 			break;
-		case LOG::INFO: 
+		case LOG::INFO:
+			std::cout << "\033[97;46m";
 			break;
-		case LOG::WARN: 
+		case LOG::WARN:
+			std::cout << "\033[43;30m";
 			break;
 		case LOG::ERROR:
+			std::cout << "\033[97;101m";
 			break;
-		case LOG::FATAL: 
+		case LOG::FATAL:
+			std::cout << "\033[97;41m";
 			break;
 		}
+	#endif
 
-		std::cout << msg.get_fullString() << std::endl;
+		std::cout << msg.get_lvlString();
+
+	#ifdef win32
+		std::cout << "\033[0m";
+	#endif
+
+		std::cout << msg.get_msgString() << std::endl;
 	}
 }
